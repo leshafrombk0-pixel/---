5c52f5aa9ea7bf362274932f7131aee67750ffb6
adfasdf <leshafrombk0@gmail.com>
2026-06-29 09:47:28 +0300

    Update pz4(1).py

--- pz4(1).py
+++ pz4(1).py
@@ -1,1 +1,27 @@
+# Дано целое число N (> 0). Найти сумму N^2 + (N+1)^2 + ... + (2N)^2
 
+n = input("Введите целое число N (> 0): ")
+while type(n) != int:
+    try:
+        n = int(n)
+    except ValueError:
+        print("Неправильно ввели!")
+        n = input("Введите целое число N (> 0): ")
+
+while n <= 0:
+    print("N должно быть больше 0!")
+    n = input("Введите целое число N (> 0): ")
+    while type(n) != int:
+        try:
+            n = int(n)
+        except ValueError:
+            print("Неправильно ввели!")
+            n = input("Введите целое число N (> 0): ")
+
+s = 0
+i = n
+while i <= 2 * n:
+    s += i * i
+    i += 1
+
+print("Сумма:", s)
